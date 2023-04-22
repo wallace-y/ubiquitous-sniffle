6d81e240ead73722e4567708117e0077b655e4f0
wallace-y <cameronwfinney@gmail.com>
2023-04-22 16:55:17 +0100

    adding find next unguessed pokemon button

--- game-logic/next-and-prev-pokemon.js
+++ game-logic/next-and-prev-pokemon.js
@@ -1,15 +1,14 @@
 import { pokemonList } from "../api-calls-and-data/pokemon-data/pokemonList.js";
 
-
 document.addEventListener("DOMContentLoaded", function () {
-  
   let pokemonHtmlImage = document.getElementById("pokemonImage");
   let currentPokemonIndex = 0;
   let currentPokemonId = 1;
   let currentPokemonName = "bulbasaur";
   let score = 0;
-  let nextNotGuessed = pokemonList.find(pokemon => pokemon.alreadyGuessed === undefined)
-
+  let nextNotGuessed = pokemonList.find(
+    (pokemon) => pokemon.alreadyGuessed === undefined
+  );
 
   function nextPokemon() {
     currentPokemonIndex++;
@@ -20,16 +19,12 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     pokemonHtmlImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/transparent/${currentPokemonId}.png`;
     fetchPokemonName();
-    displayCorrectMessage()
-    console.log(currentPokemonId, currentPokemonIndex);
-
+    displayCorrectMessage();
   }
 
   function prevPokemon() {
-    console.log(currentPokemonId, currentPokemonIndex);
     currentPokemonIndex--;
     currentPokemonId--;
-    console.log(currentPokemonId, currentPokemonIndex);
     if (currentPokemonIndex < 0) {
       currentPokemonIndex = 150;
       currentPokemonId = 151;
@@ -45,17 +40,22 @@ document.addEventListener("DOMContentLoaded", function () {
     // const data = await res.json();
     currentPokemonName = pokemonList[currentPokemonIndex].name;
     // currentPokemonName = data.results[currentPokemonIndex].name;
-
-    console.log(currentPokemonName);
   }
 
   function firstNotGuessed() {
-    nextNotGuessed = pokemonList.find(pokemon => pokemon.alreadyGuessed === undefined)
+    nextNotGuessed = pokemonList.indexOf(
+      pokemonList.find((pokemon) => pokemon.alreadyGuessed === undefined)
+    );
+    currentPokemonIndex = nextNotGuessed;
+    currentPokemonId = nextNotGuessed + 1;
+    currentPokemonName = pokemonList[currentPokemonIndex].name;
+    pokemonHtmlImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/transparent/${currentPokemonId}.png`;
+      console.log(currentPokemonId,currentPokemonIndex,currentPokemonName)
   }
 
   function guessPokemon(e) {
+    console.log(currentPokemonId,currentPokemonIndex,currentPokemonName)
     if (e.target.value === currentPokemonName) {
-
       if (pokemonList[currentPokemonIndex].alreadyGuessed === undefined) {
         //set guessed to true
         //increment score
@@ -65,26 +65,23 @@ document.addEventListener("DOMContentLoaded", function () {
       } else {
         //don't increment score
         //display already guessed message
-        console.log("Guessed")
+        console.log("Guessed");
       }
       e.target.value = "";
       nextPokemon();
-      firstNotGuessed()
-      console.log(nextNotGuessed)
-
     }
   }
 
   function setScore() {
     score++;
     document.getElementById("score").innerHTML = `Score ${score}/151`;
-    console.log(score);
   }
 
   function displayCorrectMessage() {
     const successMessage = document.getElementById("success-message");
-    console.log(pokemonList[currentPokemonIndex].alreadyGuessed)
-    pokemonList[currentPokemonIndex].alreadyGuessed === undefined ? successMessage.setAttribute("hidden",true) : successMessage.removeAttribute("hidden");
+    pokemonList[currentPokemonIndex].alreadyGuessed === undefined
+      ? successMessage.setAttribute("hidden", true)
+      : successMessage.removeAttribute("hidden");
   }
 
   document.getElementById("nextButton").addEventListener("click", nextPokemon);
@@ -92,16 +89,14 @@ document.addEventListener("DOMContentLoaded", function () {
   document
     .getElementById("pokemonGuess")
     .addEventListener("input", guessPokemon);
-    document.addEventListener("keydown", function(event) {
-      if (event.key === "ArrowLeft") {
-        // left arrow key pressed
-        prevPokemon();
-      } else if (event.key === "ArrowRight") {
-        // right arrow key pressed
-        nextPokemon();
-      }
-    });
-    
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      // left arrow key pressed
+      prevPokemon();
+    } else if (event.key === "ArrowRight") {
+      // right arrow key pressed
+      nextPokemon();
+    }
+  });
+  document.getElementById("findNext").addEventListener("click", firstNotGuessed)
 });
-
-
