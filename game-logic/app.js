4c105e9c8adc4e578fada473167f1b9de8be0041
wallace-y <cameronwfinney@gmail.com>
2023-04-22 13:35:47 +0100

    adding input field for pokemone guess

--- game-logic/app.js
+++ game-logic/app.js
@@ -4,19 +4,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// axios
-//   .get("https://pokeapi.co/api/v2/pokemon?limit=151")
-//   .then((pokemon) => {
-//     // console.log(pokemon.data.results)
-//     fsPromise.writeFile(
-//       "api-calls-and-data/pokemon-data/list-of-pokemon-names.txt",
-//       JSON.stringify(pokemon.data.results,null,2),
-//       { encoding: "utf-8" }
-//     );
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
 
 
 app.get("/", function (res, res) {
